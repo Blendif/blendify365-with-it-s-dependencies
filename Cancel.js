d238b392df6443fe2e4c5cefc99f39dc271b3410
Blendif <blendify365@gmail.com>
2024-09-14 23:20:26 +0100

    Create Cancel.js

---
+++ Cancel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cancel = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold text-center my-8">Payment Cancelled</h1>
+      <p className="text-center text-xl">Your payment was cancelled. If you have any issues, please contact support.</p>
+    </div>
+  );
+};
+
+export default Cancel;
+￼Enter
