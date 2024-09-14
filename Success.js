c84ad71d777273bf08cbeed87350810d31950d87
Blendif <blendify365@gmail.com>
2024-09-14 23:19:45 +0100

    Create Success.js

---
+++ Success.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Success = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold text-center my-8">Thank you for your purchase!</h1>
+      <p className="text-center text-xl">Your ebook is on the way. You will receive an email with the download link shortly.</p>
+    </div>
+  );
+};
+
+export default Success;
