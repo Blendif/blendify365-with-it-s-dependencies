30b7c2d8bc5a23fc8e909d541a520a1825848fc0
Blendif <blendify365@gmail.com>
2024-09-14 23:25:32 +0100

    Update App.js

--- App.js
+++ App.js
@@ -15,5 +15,25 @@ function App() {
     </Router>
   );
 }
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import EbookPage from './components/EbookPage';
+import Success from './components/Success';
+import Cancel from './components/Cancel';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/ebooks/:id" component={EbookPage} />
+        <Route path="/success" component={Success} />
+        <Route path="/cancel" component={Cancel} />
+      </Switch>
+    </Router>
+  );
+}
 
 export default App;
+
